b95ae6a31973979b0119920d1e96feba9f261a5d
NathalieMS <nathalie.moreira@hotmail.com>
2021-10-04 19:08:02 -0300

    Projeto finalizado

--- requisicao.js
+++ requisicao.js
@@ -1,46 +1,35 @@
 // https://api.nasa.gov/planetary/apod?api_key=PsnCkbYnUby5g8RYgsNgPbl92Us5lupJEnhZgFOL
 
 // Key PsnCkbYnUby5g8RYgsNgPbl92Us5lupJEnhZgFOL
-let botao = document.getElementById('button')
+
+let button = document.getElementById('button')
 let date = document.getElementById('date')
-let div = document.getElementById('conteudo')
+let conteudo = document.getElementById('containerData')
 
 
-botao.addEventListener('click', function (event) { 
+button.addEventListener('click', function (event) { 
     event.preventDefault()
     console.log('Procurando a imagem')
     search(date.value)
 })
 
 async function search (date) {
- let response =  await fetch (`https://api.nasa.gov/planetary/apod?api_key=PsnCkbYnUby5g8RYgsNgPbl92Us5lupJEnhZgFOL&date=${date}`);
-    console.log(response)
+ let resposta =  await fetch (`https://api.nasa.gov/planetary/apod?api_key=PsnCkbYnUby5g8RYgsNgPbl92Us5lupJEnhZgFOL&date=${date}`);
+    console.log(resposta)
 
- let info = await response.json()   
- console.log (info)
- console.log ('Vai mermão')
- exibir(info)
+ let content = await resposta.json()   
+ exibir(content)
 }
 
-function exibir(info) {
-
-    div.style.boxSizing = 'content-box'
-    div.style.background = 'rgb(99, 129, 212, .5)'
-    div.style.border = '5px solid white'
-    div.style.borderRadius = '4px'
-    
-    div.style.display = 'flex'
-    div.style.flexDirection = 'row'
-    div.style.justifyContent = 'space-around'
-    div.style.flexWrap = 'wrap'
+function exibir(content) {
 
 
-if (info.media_type === 'image') {
- div.innerHTML =  `<h2>${info.title} (${info.date})</h2> <p>${info.explanation}</p> <img src= "${info.url}"> `
+if (content.media_type === 'image') {
+ conteudo.innerHTML =  `<h2>${content.title} (${content.date})</h2> <p>${content.explanation}</p> <img src= "${content.url}"> `
  
 
-} else if (info.media_type === 'video') { 
-    div.innerHTML = `<h2>${info.title} (${info.date})</h2> <p>${info.explanation}</p> <iframe src=${info.url}></iframe>`
+} else if (content.media_type === 'video') { 
+    conteudo.innerHTML = `<h2>${content.title} (${content.date})</h2> <p>${content.explanation}</p> <iframe src=${content.url}></iframe>`
     
 }    
 }
